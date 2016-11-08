db5a16cb9e97075903ed296bdd6bbdf7f0f8f690
Alyssa Sinicropi <asinicropi@aim.com>
2016-11-08 16:13:00 -0500

    merp

--- app/scripts/models/orders.js
+++ app/scripts/models/orders.js
@@ -6,7 +6,6 @@ var OrderItem = Backbone.Model.extend({
 
 var OrderItemCollection = Backbone.Collection.extend({
   model: OrderItem,
-  url: 'https://tiny-lasagna-server.herokuapp.com/collections/majesticthaiorders'
   total: function(){
     return this.reduce(function(sum, model){
       return sum + parseFloat(model.get('price'));
